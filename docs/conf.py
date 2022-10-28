35b1cff3e719bc4daa561ce178f641471f735beb
Bart Thate <bthate67@gmail.com>
2022-10-28 16:44:30 +0200

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,51 @@
-# This file is placed in the Public Domain.
+# OPERBOT - ioperator bot
 # -*- coding: utf-8 -*-
+#
+
+
+"operator bot"
+
+
+__name__ = "operbot"
+__version__ = "104"
 
 
 import doctest
-import sys
 import os
+import sys
+import unittest
 
 
 curdir = os.getcwd()
 
 
-sys.path.insert(0, curdir)
-sys.path.insert(0, curdir + os.sep + '..' )
-sys.path.insert(0, curdir + os.sep + '..'  + os.sep + "..")
+sys.path.insert(0, os.path.join(curdir))
+sys.path.insert(0, os.path.join(curdir, ".."))
+sys.path.insert(0, os.path.join(curdir, "..", ".."))
 
 
-__version__ = "104"
+# -- Options for GENERIC output ---------------------------------------------
 
 
+project = __name__
+master_doc = 'index'
+version = '%s' % __version__
+release = '%s' % __version__
+language = ''
+today = ''
+today_fmt = '%B %d, %Y'
 needs_sphinx='1.7'
-
-nitpick_ignore = [
-                ('py:class', 'builtins.BaseException'),
-               ]
-
-
-extensions = [
+exclude_patterns = ['_build', '_templates', '_source', 'Thumbs.db', '.DS_Store']
+source_suffix = '.rst'
+source_encoding = 'utf-8-sig'
+modindex_common_prefix = [""]
+keep_warnings = False
+templates_path=['_templates']
+add_function_parentheses = False
+add_module_names = False
+show_authors = False
+pygments_style = 'colorful'
+extensions=[
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
@@ -35,35 +55,14 @@ extensions = [
 ]
 
 
-project = "OPERBOT"
-version = '%s' % __version__
-release = '%s' % __version__
+# -- Options for HTML output -------------------------------------------------
 
 
-html_short_title ="operator bot"
-html_title = "OPERBOT"
-html_style = 'operbot.css'
+html_title = "Prosecutor. Court. Reconsider OTP-CR-117/19."
+html_style = 'genocide.css'
 html_static_path = ["_static"]
-html_css_files = ["operbot.css",]
-html_theme = "alabaster"
-html_theme_options = {
-    'github_user': 'operbot',
-    'github_repo': 'operbot',
-    'github_button': True,
-    'github_banner': False,
-    'logo': 'opersmile.png',
-    'link': '#000',
-    'link_hover': '#000',
-    'nosidebar': True,
-    'show_powered_by': False,
-    'show_relbar_top': False,
-}
-#html_theme_path = []
-html_favicon = "opersmile.png"
-html_extra_path = []
-html_last_updated_fmt = '%Y-%b-%d'
-html_additional_pages = {}
-html_domain_indices = True
+html_css_files = ["genocide.css",]
+html_short_title = "GENOCIDE %s" % __version__
 html_sidebars = {
     '**': [
         'about.html',
@@ -72,31 +71,41 @@ html_sidebars = {
         'relations.html',
     ]
 }
-html_use_index = True
-html_split_index = True
+html_theme = "alabaster"
+html_theme_options = {
+    'github_user': 'bthate',
+    'github_repo': __name__,
+    'github_button': False,
+    'github_banner': False,
+    'logo': 'skull.jpg',
+    'link': '#000',
+    'link_hover': '#000',
+    'nosidebar': True,
+    'show_powered_by': False,
+    'show_relbar_top': False,
+    'sidebar_width': 0,
+}
+html_favicon = "skull.jpg"
+html_extra_path = []
+html_last_updated_fmt = '%Y-%b-%d'
+html_additional_pages = {}
+html_domain_indices = False
+html_use_index = False
+html_split_index = False
 html_show_sourcelink = False
 html_show_sphinx = False
 html_show_copyright = False
 html_copy_source = False
-html_use_opensearch = 'http://operbot.rtfd.io/'
+html_use_opensearch = 'http://%s.rtfd.io/' % __name__
 html_file_suffix = '.html'
-htmlhelp_basename = 'pydoc'
-templates_path = ['_templates']
-source_suffix = '.rst'
-source_encoding = 'utf-8-sig'
-master_doc = 'index'
-language = ''
-today = ''
-today_fmt = ''
-#exclude_patterns = ['_build', "_sources", "_templates"]
-exclude_patterns = ['_build', '_templates', '_source', 'Thumbs.db', '.DS_Store']
-default_role = ''
-add_function_parentheses = False
-add_module_names = False
-show_authors = True
-pygments_style = 'colorful'
-modindex_common_prefix = [""]
-keep_warnings = True
+htmlhelp_basename = 'testdoc'
+
+intersphinx_mapping = {
+                       'python': ('https://docs.python.org/3', 'objects.inv'),
+                       'sphinx': ('http://sphinx.pocoo.org/', None),
+                      }
+intersphinx_cache_limit=1
+
 rst_prolog = '''.. image:: operline.png
     :width: 100%
     :height: 2.3cm
@@ -114,21 +123,17 @@ rst_prolog = '''.. image:: operline.png
    </center>
 
 '''
-autosummary_generate = True
-autodoc_default_flags = ['members',
-                         'undoc-members',
-                         'private-members',
-                         "imported-members"]
-autodoc_member_order = 'bysource'
-autodoc_docstring_signature = False
-autoclass_content = "class"
-doctest_global_setup = ""
-doctest_global_cleanup = ""
-doctest_test_doctest_blocks = "default"
-trim_doctest_flags = True
-doctest_flags = doctest.REPORT_UDIFF
-intersphinx_mapping = {
-                       'python': ('https://docs.python.org/3', 'objects.inv'),
-                       'sphinx': ('http://sphinx.pocoo.org/', None),
-                      }
-intersphinx_cache_limit = 1
+
+autosummary_generate=True
+autodoc_default_flags=['members', 'undoc-members', 'private-members', "imported-members"]
+autodoc_member_order='groupwise'
+autodoc_docstring_signature=True
+autoclass_content="class"
+doctest_global_setup=""
+doctest_global_cleanup=""
+doctest_test_doctest_blocks="default"
+trim_doctest_flags=True
+doctest_flags=doctest.REPORT_UDIFF
+nitpick_ignore=[
+                ('py:class', 'builtins.BaseException'),
+               ]
