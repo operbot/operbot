d4227c7ef950264b08bd1fae136957bffe6ce4db
Bart Thate <operbot100@gmail.com>
2022-11-15 16:15:51 +0100

    up version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def uploadlist(dir):
 
 setup(
     name="operbot",
-    version="105",
+    version="106",
     author="Bart Thate",
     author_email="operbot100@gmail.com",
     url="http://github.com/operbot/operbot",
