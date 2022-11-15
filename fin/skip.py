718a6f2ba7d07966502c3d3f2cfbe3973945a3e1
Bart Thate <operbot100@gmail.com>
2022-11-15 17:23:17 +0100

    over to test

--- fin/skip.py
+++ fin/skip.py
@@ -12,19 +12,19 @@ SKIP = []
 
 
 try:
-    SKIP.extend([x.strip() for x in open("fin.cfg", "r").readlines()[-1].split(",")])
+    SKIP.extend([x.strip() for x in open("fin/skip.cfg", "r").readlines()[-1].split(",")])
 except:
     pass
 
 
-def doskip(txt, skip=[]):
+def doskip(txt, skip=SKIP):
     for skp in skip:
         if skp in txt:
             return True
     return False
 
 
-def popen(txt, skip=[]):
+def popen(txt, skip=SKIP):
     for line in os.popen(txt).readlines():
         if doskip(line, skip):
             continue
