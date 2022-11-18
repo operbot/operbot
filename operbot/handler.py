56f5ba7681017ab487ecbf6ca01031642ad15e0c
Bart Thate <operbot100@gmail.com>
2022-11-18 14:36:53 +0100

    update operbot

--- operbot/handler.py
+++ operbot/handler.py
@@ -12,9 +12,9 @@ import threading
 import time
 
 
-from .obj import Class, Default, Object, register, update
-from .thr import launch
-from .utl import elapsed
+from .object import Class, Default, Object, register, update
+from .thread import launch
+from .util import elapsed
 
 
 def __dir__():
@@ -33,9 +33,6 @@ def __dir__():
 
 
 __all__ = __dir__()
-
-
-Cfg = Default()
 
 
 class Bus(Object):
