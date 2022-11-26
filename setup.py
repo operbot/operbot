cf73fc0081d310eefee4513f555670821bf8fc68
Bart Thate <operbot100@gmail.com>
2022-11-26 16:07:23 +0100

    testjes and lint

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def uploadlist(dir):
 
 setup(
     name="operbot",
-    version="107",
+    version="108",
     author="Bart Thate",
     author_email="operbot100@gmail.com",
     url="http://github.com/operbot/operbot",
