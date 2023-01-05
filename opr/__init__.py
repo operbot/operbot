ba657d67580da06ccd9189d939554214053ec12d
B.H.J. Thate <thatebhj@gmail.com>
2023-01-06 00:37:14 +0100

    add opr

--- opr/__init__.py
+++ opr/__init__.py
@@ -2,13 +2,82 @@
 # pylint: disable=W0622
 
 
-"object programming runtime"
+"object programming"
 
 
-from opr import handler, objects, running, threads
+from opr import message, handler, objects, runtime, threads
 
 
+from opr.message import *
 from opr.handler import *
 from opr.objects import *
-from opr.running import *
+from opr.runtime import *
 from opr.threads import *
+
+
+def __dir__():
+    return (
+            'Bus',
+            'Callback',
+            'Cfg',
+            'Class',
+            'Command',
+            'Config',
+            'Db',
+            'Default',
+            'Event',
+            'Handler',
+            'NoUser',
+            'Object',
+            'ObjectDecoder',
+            'ObjectEncoder',
+            'Parsed',
+            'Repeater',
+            'Thread',
+            'Timer',
+            'Wd',
+            'boot',
+            'cdir',
+            'command',
+            'dump',
+            'dumps',
+            'edit',
+            'elapsed',
+            'find',
+            'fns',
+            'fntime',
+            'hook',
+            'include',
+            'items',
+            'keys',
+            'kind',
+            'last',
+            'launch',
+            'listmod',
+            'load',
+            'loads',
+            'locked',
+            'match',
+            'modules',
+            'name',
+            'parse',
+            'printable',
+            'register',
+            'save',
+            'scandir',
+            'scanpkg',
+            'spl',
+            'update',
+            'values',
+            'wait',
+            'write',
+            'message',
+            'handler',
+            'objects',
+            'runtime',
+            'threads',
+            'usersdb'
+           )
+
+
+__all__ = __dir__()
